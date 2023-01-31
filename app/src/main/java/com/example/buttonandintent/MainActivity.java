ddfb5f6b143adc2e1079adabb16db4a63239caa9
Ismaeel Mughal <ismaeel.mughal.1912@gmail.com>
2023-02-01 02:45:49 +0500

    Git On The Go

---
+++ app/src/main/java/com/example/buttonandintent/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.buttonandintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button gitBtn, addStudentBtn, assignSabakBtn, showSabak;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gitBtn = findViewById(R.id.gitBtn);
+        addStudentBtn = findViewById(R.id.addStudentBtn);
+        assignSabakBtn = findViewById(R.id.assignSabakBtn);
+        showSabak = findViewById(R.id.showSabak);
+
+        showSabak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StudentRecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        assignSabakBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddSabakActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addStudentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://github.com/IsmaeelMughal/MadrissaSabak/commits/master");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
+    }
+}
